6f659528cd0fd081eaee69e08d427e25d69860f4
Rafael Dias <faaell@gmail.com>
2024-04-02 01:43:13 -0300

    added-final-layout

--- calculator.py
+++ calculator.py
@@ -1,6 +1,28 @@
 import flet as ft
 from flet import colors
 
+bottons = [
+    {'operator': 'AC', 'font': colors.BLACK, 'bottom': colors.BLUE_GREY_100},
+    {'operator': '±', 'font': colors.BLACK, 'bottom': colors.BLUE_GREY_100},
+    {'operator': '%', 'font': colors.BLACK, 'bottom': colors.BLUE_GREY_100},
+    {'operator': '/', 'font': colors.WHITE, 'bottom': colors.ORANGE},
+    {'operator': '7', 'font': colors.WHITE, 'bottom': colors.WHITE24},
+    {'operator': '8', 'font': colors.WHITE, 'bottom': colors.WHITE24},
+    {'operator': '9', 'font': colors.WHITE, 'bottom': colors.WHITE24},
+    {'operator': '*', 'font': colors.WHITE, 'bottom': colors.ORANGE},
+    {'operator': '4', 'font': colors.WHITE, 'bottom': colors.WHITE24},
+    {'operator': '5', 'font': colors.WHITE, 'bottom': colors.WHITE24},
+    {'operator': '6', 'font': colors.WHITE, 'bottom': colors.WHITE24},
+    {'operator': '-', 'font': colors.WHITE, 'bottom': colors.ORANGE},
+    {'operator': '1', 'font': colors.WHITE, 'bottom': colors.WHITE24},
+    {'operator': '2', 'font': colors.WHITE, 'bottom': colors.WHITE24},
+    {'operator': '3', 'font': colors.WHITE, 'bottom': colors.WHITE24},
+    {'operator': '+', 'font': colors.WHITE, 'bottom': colors.ORANGE},
+    {'operator': '0', 'font': colors.WHITE, 'bottom': colors.WHITE24},
+    {'operator': '.', 'font': colors.WHITE, 'bottom': colors.WHITE24},
+    {'operator': '=', 'font': colors.WHITE, 'bottom': colors.ORANGE}
+]
+
 def main(page: ft.Page):
     page.bgcolor = '#000'
     page.window_resizable = False
@@ -8,15 +30,32 @@ def main(page: ft.Page):
     page.window_height = 380
     page.title = 'Calculator'
     page.window_always_on_top = True
+    page.window_left = 0
 
-    result = ft.Text(value = '0', color = colors.WHITE, size=20)
+    result = ft.Text(value ='0', color=colors.WHITE, size=20)
 
     display = ft.Row(
         width=250,
         controls=[result],
-        alignment= 'end',
+        alignment='end',
     )
 
-    page.add(display)
+    btn = [ft.Container(
+            content=ft.Text(value=btn['operator'], color=btn['font']),
+            width=50,
+            height=50,
+            bgcolor=btn['bottom'],
+            border_radius=100,
+            alignment=ft.alignment.center
+        ) for btn in bottons]
+
+    keyboard = ft.Row(
+        width=250,
+        wrap=True,
+        controls=btn,
+        alignment='end'
+    )
+
+    page.add(display, keyboard)
 
 ft.app(target = main)
